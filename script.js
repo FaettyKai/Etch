93f3c58ee3c0820800bc9ff04a5c5b3a191fa114
Michael <140651581+FaettyKai@users.noreply.github.com>
2023-10-05 23:55:11 -0500

    change color on click

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 container = document.querySelector("#container")
 container.setAttribute("style", "width: 28em; height: 32em")
 
+button = " "
+buttonDown = "false"
 canvas = () => {
     for(let i = 0; i <= 255; i++) {
         button = document.createElement("button")
         button.setAttribute("style", "width: 2em; height: 2em;")
+        button.classList.add("content")
+        button.textContent = ""
         container.appendChild(button)
-
+        button.addEventListener("mousedown", () => {
+            buttonDown = "true"
+            button.setAttribute("style", "background: blue;")
+        })
+        button.addEventListener("mouseup", () => {
+            buttonDown = "false"
+        })
     }
 }
+
 canvas()
 
