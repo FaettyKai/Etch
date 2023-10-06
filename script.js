57b7be88d022bf94b41162c6aacbbf360dc0b23e
Michael <140651581+FaettyKai@users.noreply.github.com>
2023-10-06 00:09:16 -0500

    solution?

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ container = document.querySelector("#container")
 container.setAttribute("style", "width: 28em; height: 32em")
 
 button = " "
-buttonDown = "false"
 canvas = () => {
     for(let i = 0; i <= 255; i++) {
         button = document.createElement("button")
@@ -10,15 +9,38 @@ canvas = () => {
         button.classList.add("content")
         button.textContent = ""
         container.appendChild(button)
-        button.addEventListener("mousedown", () => {
-            buttonDown = "true"
-            button.setAttribute("style", "background: blue;")
-        })
-        button.addEventListener("mouseup", () => {
-            buttonDown = "false"
-        })
     }
+    color()
 }
 
 canvas()
 
+color = () => {
+        button.addEventListener("mousedown", () => {
+            button.setAttribute("style", "background: blue; width: 2em; height: 2em;")
+        })
+} 
+
+/*
+const container = document.querySelector("#container");
+
+        function createButton() {
+            const button = document.createElement("button");
+            button.classList.add("content");
+
+            button.addEventListener("mousedown", () => {
+                button.style.background = "blue";
+            });
+
+            return button;
+        }
+
+        function createCanvas() {
+            for (let i = 0; i < 256; i++) {
+                const button = createButton();
+                container.appendChild(button);
+            }
+        }
+
+        createCanvas();
+*/
