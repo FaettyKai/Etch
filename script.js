892869b53f770d99dadd43ecf814201f97dd65b2
Michael <140651581+FaettyKai@users.noreply.github.com>
2023-10-10 00:21:02 -0500

    Set canvas size. Add Clear function. Normal function works arrow doesn't (???)

--- script.js
+++ script.js
@@ -6,15 +6,24 @@ canvas = []
 
 for(var i;i<255; i++) {
     button = document.createElement("button")
-    button.setAttribute("style", "height: 2em; width: 2em;")
+    button.setAttribute("style", "height: 2em; width: 2em; background: white;")
     canvas.push(button)
     container.appendChild(button)
 }
 
 color = (button) => {
-	button.addEventListener("mousedown", () => {
-  	button.setAttribute("style", "background: blue;")
+	button.addEventListener("mouseover", () => {
+  	button.setAttribute("style", "background: blue; height: 2em; width: 2em;")
   })
 }
 
 canvas.forEach(color)
+
+clear = document.querySelector("#clear")
+clear.addEventListener("click", clearCanvas)
+
+function clearCanvas() {
+	canvas.forEach((button) => {
+  	button.setAttribute("style", "height: 2em; width: 2em; background: white;")
+  })
+}
